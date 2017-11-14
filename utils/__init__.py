14a65bee703270b3ce20a283fb26e8d0d7018972
Andre Voges <apvoges@gmail.com>
2017-11-14 15:59:52 +0200

    MIT BDSA P5 M4 update

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,3 +3,4 @@ from utils.geo import llaToECEF
 from utils.geo import ECEFTolla
 from utils.median import getmedian
 from utils.graph import draw_partitioned_graph
+from utils.fancy_dendrogram import fancy_dendrogram
